b5c7feaf7a425b4a6d32500a2fa54aa88f70210f
agsrc <akshay0712@gwu.edu>
2020-07-26 09:46:56 -0400

    intial commit

---
+++ src/test/java/edu/agsrc/mobile/MobileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.agsrc.mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
